bc5f07eb8a4bbb4bc9389f68ef86a019307c09ae
Arianne Hernandez <40613380+afloreshernandez@users.noreply.github.com>
2019-03-28 21:09:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace christmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+               
+                for (int i = 1; i <= 10; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+
+
+                        Console.Write("*");
+
+
+                    }
+
+                    Console.Write("\n");
+                }
+            
+
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+
+                    
+                    Console.Write("+");
+                    
+
+                }
+
+                Console.Write("\n");
+            }
+
+
+        }
+    }
+            
+        }
+    
+
